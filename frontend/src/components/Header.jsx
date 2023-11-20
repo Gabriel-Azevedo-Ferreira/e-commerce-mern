8d3142064f6c0367f0fee4baf55017fa901b3d8c
gabriel <az.fe.gabriel@gmail.com>
2023-11-20 22:33:08 +0800

    logout and login

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { Nav, Navbar, Container, Badge } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { setCredentials } from "../slice/authSlice";
+import { useLogoutMutation } from "../slice/usersApiSlice";
+
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
   const { userInfo } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [logout] = useLogoutMutation();
+
+  const loginOutHandler = async () => {
+    if (loggedIn) {
+      await logout();
+      dispatch(setCredentials(null));
+    } else navigate("/login");
+  };
+
   const loggedIn = !!userInfo;
   return (
     <header>
@@ -31,11 +47,9 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to={loggedIn ? "/logout" : "/login"}>
-                <Nav.Link>
-                  <FaUser /> {loggedIn ? "Sign out" : "Sign In"}
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link onClick={loginOutHandler}>
+                <FaUser /> {loggedIn ? "Sign out" : "Sign In"}
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
